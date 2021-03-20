06639e1434e80b99ec0ee5b572352fd6bd9eb1cc
DESKTOP-9SFLGPG\kv_ka <kannan@logicworksbt.com>
2021-03-20 15:40:41 +0530

    Test

---
+++ MySelenium/src/main/java/Week1/Day1/FirstTest.java
@@ -0,0 +1,18 @@
+package Week1.Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.moneycontrol.com/fixed-income/calculator/state-bank-of-india/fixed-deposit-calculator-SBI-BSB001.html?classic=true");
+		
+		
+		
+
+	}
+
+}
